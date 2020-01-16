506db4aed4f5cc14ec6d09e9b73d1a3ea6432a0b
Khalil Daoulat <48126333+Vvoox@users.noreply.github.com>
2020-01-17 00:34:03 +0100

    Add files via upload

--- sample/Main.java
+++ sample/Main.java
@@ -1,17 +1,23 @@
-package sample;
+package application;
 
+
+import application.views.Home;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
+
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("HOME.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+//        BorderPane root = new BorderPane();
+
+        Parent root = FXMLLoader.load(getClass().getResource("views/HOME.fxml"));
         Image icon = new Image(getClass().getResourceAsStream("Images/ensa1.png"));
         primaryStage.getIcons().add(icon);
         primaryStage.setTitle("ENSAHub");
@@ -19,15 +25,7 @@ public class Main extends Application {
         primaryStage.show();
 
     }
-    /*public static void start1(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("AFFINE_CIPHER.fxml"));
-        Image icon = new Image(getClass().getResourceAsStream("Images/1.png"));
-        primaryStage.getIcons().add(icon);
-        primaryStage.setTitle("CRYPTOSIS");
-        primaryStage.setScene(new Scene(root, 1200, 700));
-        primaryStage.show();
 
-    }*/
 
     public static void main(String[] args) {
         launch(args);
